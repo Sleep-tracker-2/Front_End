5ad7467a4f8b427694fe71305227999203b603cf
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-02-26 23:17:50 -0500

    add splash page

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Container, Button } from "@material-ui/core";
+
+const splashStyle = {
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	width: "100vw",
+	height: "100vh",
+	backgroundColor: "#1b262c",
+	color: "black",
+	flexDirection: "column"
+};
+
+const buttonStyle = {
+	color: "black",
+	borderColor: "black"
+};
 
 function Splash() {
 	return (
-		<Container
-			style={{
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center"
-			}}
-		>
-			<h1>Sleep!</h1>
-		</Container>
+		<div style={splashStyle} className='splash'>
+			<h1 className='splashTitle'>Sleep is Good! Get it!</h1>
+			<Button style={buttonStyle} variant='outlined'>
+				Track your Sleep >>
+			</Button>
+		</div>
 	);
 }
 
