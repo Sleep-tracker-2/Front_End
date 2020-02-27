13f8e762fcae1e44bbcf020c898af572bd1c8660
ddhorstman <davidzilla27@gmail.com>
2020-02-27 18:56:34 -0500

    Initial set-up of MaterialUI form w/o Formik

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -1,78 +1,48 @@
-import React from "react";
-import { withFormik, Form, Field } from "formik";
-import * as yup from "yup";
-import axios from "axios";
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
 
+export default function MaterialUIPickers() {
+    // The first commit of Material-UI
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
 
-function SleepEntryForm({ errors, touched, status, submitCount }) {
-    /*const [users, setUsers] = useState([]);
-    useEffect(() => {
-        status && setUsers([...users, status]);
-    }, [status]);*/
+    const handleDateChange = date => {
+        setSelectedDate(date);
+    };
+
     return (
-        <div>
-            <Form className="user-form">
-                <h3>New User Sign-Up</h3>
-                <label>Name:
-                <Field name="name"
-                        className={
-                            (submitCount > 0 && errors.name)
-                                ? "error"
-                                : ""
-                        } />
-                    {
-                        (submitCount > 0 && errors.name)
-                        && <p className="error-message">{errors.name}</p>
-                    }
-                </label>
-            </Form>
-        </div>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around">
+                <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Date picker inline"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change date',
+                    }}
+                />
+                <KeyboardTimePicker
+                    margin="normal"
+                    id="time-picker"
+                    label="Time picker"
+                    value={selectedDate}
+                    onChange={handleDateChange}
+                    KeyboardButtonProps={{
+                        'aria-label': 'change time',
+                    }}
+                />
+            </Grid>
+        </MuiPickersUtilsProvider>
     );
 }
-
-export default withFormik({
-    mapPropsToValues: () => ({
-        date: "",
-        startTime: "",
-        endTime: "",
-        mood: "",
-        comment: ""
-    }),
-   /* validationSchema: yup.object().shape({
-        name: yup
-            .string()
-            .required("Please enter your name"),
-        email: yup
-            .string()
-            .required("Please enter your email address")
-            .email("Please enter a valid email address"),
-        password: yup
-            .string()
-            .required("Please enter a password")
-            .min(8, "Must be at least 8 characters long.")
-            .notOneOf([yup.ref('name'), yup.ref('email')], "Cannot match name or email."),
-        confirmPassword: yup
-            .string()
-            .required("Please confirm your password")
-            .oneOf([yup.ref('password')], "Entered passwords do not match."),
-        acceptedTOS: yup
-            .boolean()
-            .oneOf([true], "You must accept the terms of service.")
-    }),
-    handleSubmit: (values, { resetForm, setStatus, setSubmitting }) => {
-        setSubmitting(true);
-        axios
-            .post("https://reqres.in/api/users/",
-                {
-                    ...values,
-                    confirmPassword: null
-                })
-            .then(res => {
-                //console.log(res);
-                setStatus(res.data);
-                setSubmitting(false);
-                resetForm();
-            })
-            .catch(console.error);
-    }*/
-})(SleepEntryForm);
