8e4b62d9b3607a991f637e4b9007e6b9dfec7b6d
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-02-27 21:21:45 -0500

    add context

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Container, Button } from "@material-ui/core";
+import { Container, Button, ButtonGroup } from "@material-ui/core";
+import { MemoryRouter as Router } from "react-router";
+import { Link as RouterLink } from "react-router-dom";
 
 const splashStyle = {
 	display: "flex",
@@ -17,13 +19,24 @@ const buttonStyle = {
 	borderColor: "black"
 };
 
+const LinkBehavior = React.forwardRef((props, ref) => (
+	<RouterLink ref={ref} to='/graph' {...props} />
+));
+
 function Splash() {
 	return (
 		<div style={splashStyle} className='splash'>
 			<h1 className='splashTitle'>Sleep is Good! Get it!</h1>
-			<Button style={buttonStyle} variant='outlined'>
-				Track your Sleep >>
-			</Button>
+			<ButtonGroup
+				variant='text'
+				color='primary'
+				aria-label='text primary button group'
+				style={buttonStyle}
+			>
+				<Button>One</Button>
+				<Button>Two</Button>
+				<Button>Three</Button>
+			</ButtonGroup>
 		</div>
 	);
 }
