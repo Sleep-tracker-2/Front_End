d8c1aafe6d1416310be0f6b7407233818f4f70d2
Jason Cook <the.root.user@gmail.com>
2020-03-04 23:12:32 -0500

    Implement Justify-Content inline

--- src/components/UserDash.js
+++ src/components/UserDash.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import SleepGraphContainer from './SleepGraphContainer';
 import NewEntry from './NewEntry.js';
-import { Container, List, ListItem } from '@material-ui/core';
+import {  List, ListItem, Paper, Typography } from '@material-ui/core';
 import { initialState } from '../reducers';
+
 const UserDash = () => {
     return (
         <>
-            <Container maxWidth='md'>
+            <Paper maxWidth='md'>
                 <SleepGraphContainer />
-                <List id='sleep-list'>
-                    {initialState.sleep.map(sleep => {
+                <List id='sleepList'>
+                    {initialState.sleep.data.map(sleep => {
                         return (
                             <ListItem
-                                className='sleep-list-item'
+                                style={{ justifyContent: 'space-around' }}
                                 key={Math.random() * 99 + 1}
                             >
-                                <p className='sleep-data'>Date: {sleep.day}</p>
-                                <p className='sleep-data'>
+                                <Typography variant='p' className='sleep-data'>
+                                    Date: {sleep.day}
+                                </Typography>
+                                <Typography variant='p' className='sleep-data'>
                                     Hours: {sleep.hours}
-                                </p>
-                                <p className='sleep-data'>Mood: {sleep.mood}</p>
+                                </Typography>
+                                <Typography variant='p' className='sleep-data'>
+                                    Mood: {sleep.mood}
+                                </Typography>
                             </ListItem>
                         );
                     })}
                 </List>
-            </Container>
+            </Paper>
         </>
     );
 };
