4179240f84c077286835c66f9e4734788e31ae7d
ddhorstman <davidzilla27@gmail.com>
2020-03-05 00:23:38 -0500

    Fixed conditional rendering in SleepGraphDetail

--- src/components/SleepDetail.js
+++ src/components/SleepDetail.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Container from '@material-ui/core/Container';
-export default function SleepDetail(props){
+export default function SleepDetail(props) {
     console.log(props);
-return (
-    <Container>
-    <Typography variant="h4">{props.day}</Typography>
-    <Typography variant="p">{"Hours: "+props.hours}</Typography>
-</Container>
-);
+    return props.day ? (
+        <Container>
+            <Typography variant="h4">{props.day}</Typography>
+            <Typography variant="p">{"Hours: " + props.hours}</Typography>
+        </Container>
+    )
+        : <div />;
 }
