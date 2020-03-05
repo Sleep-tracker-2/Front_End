6bc291cd4f9c5ee7e48e9ea8e340a04f787dc8dd
ddhorstman <davidzilla27@gmail.com>
2020-03-04 21:52:42 -0500

    Sleep Graph modal.

--- src/components/SleepGraph.js
+++ src/components/SleepGraph.js
@@ -8,9 +8,26 @@ import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
+import SleepDetail from "./SleepDetail";
 const moods = ["😡", "😔", "😐", "😄"]
 
+const useStyles = makeStyles(theme => ({
+    modal: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: "2px solid #000",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3)
+    }
+}));
+
 export default function SleepGraph({ data, showHours, showMood }) {
+    const classes=useStyles();
     function handleCloseModal(){
         setDateModal(!dateModal);
     }
@@ -117,7 +134,7 @@ export default function SleepGraph({ data, showHours, showMood }) {
             <Modal
                 aria-labelledby='transition-modal-title'
                 aria-describedby='transition-modal-description'
-                //className={classes.modal}
+                className={classes.modal}
                 open={dateModal}
                 onClose={handleCloseModal}
                 closeAfterTransition
@@ -128,7 +145,7 @@ export default function SleepGraph({ data, showHours, showMood }) {
             >
                 <Fade in={dateModal}>
                     <Paper elevation={3}>
-                        <Button >Hi!</Button>
+                        <SleepDetail />
                     </Paper>
                 </Fade>
             </Modal>
